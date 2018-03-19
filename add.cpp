d1c3d97eaba99b404f51e7578acd72e4dfdc2b46
Prateer Kumar Roy <prateergamer@gmail.com>
2018-03-19 17:55:56 +0530

    C++ code to add two numbers.

---
+++ add.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	int integer_one = 5;
+	int integer_two = 9;
+
+	int sum = integer_one + integer_two;
+
+	cout<<sum<<endl;
+
+	return 0;
+}
